66d5d32da8e5151a3a76fbce4026edb9d6bcd278
Jeanclaude09-dev <jeanclaudenkongolo7@gmail.com>
2026-01-22 22:32:24 +0300

    ft(hover/focus-state)

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
       <section className="flex justify-between items-center mt-4">
         <h1 className="text-3xl font-bold">Extensions List</h1>
         <div className="flex gap-1">
-          <button className="bg-neutral-0 px-5 py-2.5 text-lg rounded-3xl cursor-pointer">
+          <button className="bg-neutral-0 px-5 py-2.5 text-lg rounded-3xl cursor-pointer hover:opacity-85 transition-all duration-200">
             All
           </button>
-          <button className="bg-neutral-0 px-5 py-2.5 text-lg rounded-3xl cursor-pointer">
+          <button className="bg-neutral-0 px-5 py-2.5 text-lg rounded-3xl cursor-pointer hover:opacity-85 transition-all duration-200">
             Active
           </button>
-          <button className="bg-neutral-0 px-5 py-2.5 text-lg rounded-3xl cursor-pointer">
+          <button className="bg-neutral-0 px-5 py-2.5 text-lg rounded-3xl cursor-pointer hover:opacity-85 transition-all duration-200">
             Inactive
           </button>
         </div>
--- src/component/extensions.jsx
+++ src/component/extensions.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import itemsData from "../data/data.json";
+
+const [toggled, setToggled] = useState(item.isActive);
 
 function Extensions() {
   return (
@@ -8,15 +11,22 @@ function Extensions() {
           <div className="flex gap-3 items-start">
             <img src={item.logo} alt={item.alt} />
             <div>
-              <h2 className="text-lg font-bold mb-0.5">{item.name}</h2>
+              <h2 className="text-lg text-neutral-900 font-bold mb-0.5">
+                {item.name}
+              </h2>
               <p className="text-base text-neutral-500">{item.description}</p>
             </div>
           </div>
           <div className="flex justify-between items-center">
-            <button className="px-3 py-1.5 rounded-2xl border border-neutral-500 cursor-pointer">
+            <button className="text-neutral-900 px-3 py-1.5 rounded-2xl border border-neutral-500 cursor-pointer hover:bg-red-500 hover:text-neutral-0 hover:border-red-500 focus:bg-neutral-0 focus:border-red-500 focus:text-neutral-900 transition-all duration-200 ">
               Remove
             </button>
-            <input type="checkbox" id="checkbox" />
+            <button
+              className={`toggle-btn ${toggled ? "toggled" : ""}`}
+              onClick={() => setToggled(!toggled)}
+            >
+              <div className="thumb"></div>
+            </button>
           </div>
         </article>
       ))}
